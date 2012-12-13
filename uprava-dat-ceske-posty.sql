749b1db50bc7ca73166bfc866238cb461bc4ef12
Jan Zatloukal <zatloukal.jan@gmail.com>
2012-12-13 09:01:38 +0100

    v 0.3
    Opraven SQL dotaz pro přiřazení kraje k PSČ

--- uprava-dat-ceske-posty.sql
+++ uprava-dat-ceske-posty.sql
@@ -3,4 +3,4 @@ UPDATE psc SET nazev_okres='Praha' WHERE nazev_okres LIKE 'Hlavní město Praha'
 -- přiřazení okresu
 UPDATE psc a, okres b SET a.okres_id=b.id WHERE a.nazev_okres=b.nazev;
 -- přiřazení kraje
-UPDATE psc a, okres b SET a.kraj_id=b.kraj_id;
+UPDATE psc a, okres b SET a.kraj_id=b.kraj_id WHERE a.okres_id=b.id;
